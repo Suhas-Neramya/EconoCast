851d70c45fea71b2d58930aab6dda650e4c3e808
Dinuwara22 <Dinuwara22@gmail.com>
2023-02-14 20:04:11 +0530

    Initialize dataset2 Folder

--- DataScience/dataSet2/main.py
+++ DataScience/dataSet2/main.py
@@ -7,27 +7,27 @@ fields = ['News', 'Date']
 
 
 def getNews():
-    headLine = news.find('h2', class_='post-title').text
-    date = news.find('span', class_='tie-date').text
+    headLine = news.find('h2', class_='categoryArticle__title').text
+    date = news.find('p', class_='categoryArticle__meta').text.split("at")
 
-    return [headLine, date]
+    return [headLine, date[0]]
 
 
 def createURL(pageNumber):
-    return 'https://www.hellenicshippingnews.com/category/oil-energy/oil-companies-news/page/' + pageNumber + '/'
+    return 'https://oilprice.com/Latest-Energy-News/World-News/Page-' + pageNumber + '.html'
 
 
 with open('dataset.csv', 'w',encoding='utf-8', newline='') as csvFile:
     csvWriter = csv.writer(csvFile)
     csvWriter.writerow(fields)
 
-    for i in tqdm(range(2, 1354)):
+    for i in tqdm(range(2, 11)):
         number = str(i);
 
         page = requests.get(createURL(number))
 
         soup = BeautifulSoup(page.content, 'html.parser')
-        newsList = soup.find_all('article', class_='item-list')
+        newsList = soup.find_all('div', class_='categoryArticle')
 
         for news in newsList:
             csvWriter.writerow(getNews())
