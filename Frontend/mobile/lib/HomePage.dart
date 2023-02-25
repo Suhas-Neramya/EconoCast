0cb7d304f47ab0d099d628ab0b1d46e8a90a387b
IrushiJayasekara <irushi.20211319@iit.ac.lk>
2023-02-25 23:07:07 +0530

    mobile-Added Icon and  a chart
    Added icons to the App Bar and added a chart

--- Frontend/mobile/lib/HomePage.dart
+++ Frontend/mobile/lib/HomePage.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 void main() => runApp(const AppBarApp());
 
@@ -17,12 +17,15 @@ class AppBarApp extends StatelessWidget{
 }
 
 class HomePage extends StatelessWidget {
+  final _htmlContent = """
+  <iframe title="Oil Prices Blend Chart" scrolling="no" src="https://oilprice.com/freewidgets/get_oilprices_chart/45/4" style="height: 1000; width: 1000"></iframe>
+  """;
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amberAccent,
+
         appBar: AppBar(
         backgroundColor: Colors.amber,
 
@@ -30,9 +33,77 @@ class HomePage extends StatelessWidget {
 
         title: const Text('Econo'),
     foregroundColor: Colors.brown,
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.access_time_sharp),
+                color: Colors.brown,
+                tooltip: 'Show Snackbar',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('This is a snackbar')));
+                },
+              ),
+
+              IconButton(
+                icon: const Icon(Icons.add_alert),
+                color: Colors.brown,
+                tooltip: 'Show notification',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('notification')));
+                },
+              ),
+
+              IconButton(
+                icon: const Icon(Icons.account_circle_rounded),
+                color: Colors.brown,
+                tooltip: 'Show contact',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('contact')));
+                },
+              ),
+
+              IconButton(
+                icon: const Icon(Icons.navigate_next),
+                tooltip: 'Go to the next page',
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute<void>(
+                    builder: (BuildContext context) {
+                      return Scaffold(
+                        appBar: AppBar(
+                          backgroundColor: Colors.amber,
+                          title: const Text('Econo'),
+                          foregroundColor: Colors.brown,
+                        ),
+                        body: Container(
+                          color:Colors.amber,
+
+                        ),
+                      );
+                    },
+                  ));
+                },
+              ),
+
+
+            ]
 
         ),
 
+      body:  SafeArea(
+        child: SizedBox(
+          height: 4000,
+          width: 1000,
+          child: Html(
+            data: _htmlContent,
+            // Styling with CSS (not real CSS)
+            style: {
+              'iframe': Style(height: 5000, width: 1000),
+            },
+          ),
+        ),
+      ),
 
     );
 
