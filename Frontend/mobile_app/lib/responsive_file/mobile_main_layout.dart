5b034d72441322be42d7442d02cd842f576c0cf0
IrushiJayasekara <irushi.20211319@iit.ac.lk>
2023-03-16 14:53:19 +0530

    Side Navigation Bar
    Added navigation bar to the home page

--- Frontend/mobile_app/lib/responsive_file/mobile_main_layout.dart
+++ Frontend/mobile_app/lib/responsive_file/mobile_main_layout.dart
@@ -25,31 +25,12 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: const NavDrawer(),
         appBar: AppBar(
           backgroundColor: Colors.white,
           title:  Image.asset('assets/logo.png', width: 300,height: 70,alignment: Alignment.topLeft,),
           foregroundColor: Colors.brown,
           actions: <Widget>[
-
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              color: Colors.brown,
-              tooltip: 'Show notification',
-              onPressed: () {Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      backgroundColor: Colors.amber,
-                      title: const Text('Econo'),
-                      foregroundColor: Colors.brown,
-                    ),
-                    body: Container(
-                    ),
-                  );
-                },
-              ));
-              },
-            ),
 
             IconButton(
               icon: const Icon(Icons.account_circle_rounded),
@@ -86,6 +67,116 @@ class MainPage extends StatelessWidget {
             ),
           ),
         )
+    );
+  }
+}
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/logo-nav.jpg'))), child: null,
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.home_filled),
+            title: const Text('Home'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: const Icon(Icons.perm_device_information),
+            title: const Text('About'),
+            onTap: ()  {Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.amber,
+                    title: const Text('Econo'),
+                    foregroundColor: Colors.brown,
+                  ),
+                  body: Container(
+
+                  ),
+                );
+              },
+            ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.newspaper_rounded),
+            title: const Text('News'),
+            onTap: () {Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.amber,
+                    title: const Text('Econo'),
+                    foregroundColor: Colors.brown,
+                  ),
+                  body: Container(
+
+                  ),
+                );
+              },
+            ));
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.notification_add),
+            title: const Text('Notification'),
+            onTap: () {Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.amber,
+                    title: const Text('Econo'),
+                    foregroundColor: Colors.brown,
+                  ),
+                  body: Container(
+
+                  ),
+                );
+              },
+            ));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.history_outlined),
+            title: const Text('History'),
+            onTap: () {Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.amber,
+                    title: const Text('Econo'),
+                    foregroundColor: Colors.brown,
+                  ),
+                  body: Container(
+
+                  ),
+                );
+              },
+            ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
     );
   }
 }
