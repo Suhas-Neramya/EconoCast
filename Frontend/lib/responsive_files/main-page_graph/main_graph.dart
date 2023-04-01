bb8ec741f1491409671b20869b4713b3f24d27af
nisalya <fernandonisalya@gmail.com>
2023-04-01 22:45:04 +0530

    chart

--- Frontend/lib/responsive_files/main-page_graph/main_graph.dart
+++ Frontend/lib/responsive_files/main-page_graph/main_graph.dart
@@ -1,1 +1,16 @@
 import 'package:switch_up/switch_up.dart';
+import 'package:flutter/material.dart';
+
+class MainPageGraph extends StatefulWidget {
+  const MainPageGraph({super.key});
+
+  @override
+  State<MainPageGraph> createState() => _MainPageGraphState();
+}
+
+class _MainPageGraphState extends State<MainPageGraph> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
