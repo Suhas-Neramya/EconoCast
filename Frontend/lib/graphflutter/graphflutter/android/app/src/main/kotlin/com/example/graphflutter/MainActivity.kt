ede8169e3c84880fda8f1a7ed23d2fed7a004848
inukaperera <102431600+inukaperera@users.noreply.github.com>
2023-04-03 00:49:40 +0530

    setup all the things

---
+++ Frontend/lib/graphflutter/graphflutter/android/app/src/main/kotlin/com/example/graphflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.graphflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ graphuml/android/app/src/main/kotlin/com/example/graphuml/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.graphuml
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
