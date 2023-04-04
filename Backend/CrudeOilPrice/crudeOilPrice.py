1b081748ad11302915fdce2536d2644c924b434a
dinuwara22 <dinuwara22@gmail.com>
2023-04-04 15:29:55 +0545

    Completed getMonthlyPrice function

--- Backend/CrudeOilPrice/crudeOilPrice.py
+++ Backend/CrudeOilPrice/crudeOilPrice.py
@@ -16,3 +16,10 @@ def getMonthlyPrice():
     url = 'https://www.alphavantage.co/query?function=BRENT&interval=monthly&apikey=VJ0EKFU4FBWNT7EG'
     r = requests.get(url)
     data = r.json()
+
+    monthlyPriceArray = []
+
+    for record in data['data']:
+        monthlyPriceArray.append(record)
+
+    return monthlyPriceArray
