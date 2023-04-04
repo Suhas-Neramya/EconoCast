0673cb84a7f20f4c57a63a92154c3939b8c558fb
dinuwara22 <dinuwara22@gmail.com>
2023-04-04 15:20:34 +0545

    Created weeklyPriceArray and Values were assigned to it

--- Backend/CrudeOilPrice/crudeOilPrice.py
+++ Backend/CrudeOilPrice/crudeOilPrice.py
@@ -4,3 +4,10 @@ def getWeekPrice():
     url = 'https://www.alphavantage.co/query?function=BRENT&interval=weekly&apikey=VJ0EKFU4FBWNT7EG'
     r = requests.get(url)
     data = r.json()
+
+    weeklyPriceArray = []
+
+    for i in range(900):
+        weeklyPriceArray.append(data['data'][i])
+    
+    
