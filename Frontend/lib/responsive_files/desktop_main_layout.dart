b69d46d4fcab030d042c0e0cd565004e3e8a9159
Suhas Chandrasekara <98030506+Suhas-Neramya@users.noreply.github.com>
2023-04-06 01:49:20 +0530

    Update desktop_main_layout.dart

--- Frontend/lib/responsive_files/desktop_main_layout.dart
+++ Frontend/lib/responsive_files/desktop_main_layout.dart
@@ -19,17 +19,20 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:econo_cast/responsive_files/DataModel/DataModel.dart';
 import 'package:econo_cast/styles/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:switch_up/switch_up.dart';
 
-import 'desktop_login_layout.dart';
 import '../constants/dropdown_button.dart';
+import 'desktop_login_layout.dart';
 import '../news_app_mobile/api_service.dart';
 import '../news_app_mobile/article_mobile.dart';
 import '../news_app_mobile/customListTile.dart';
 import '../news_app_mobile/desktop_news_page.dart';
 import '../news_app_mobile/news_page.dart';
+
 import 'main-page_graph/main_graph.dart';
 
 import 'package:syncfusion_flutter_charts/charts.dart';
